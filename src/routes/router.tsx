620cace7c999e6c400bb3e2ac4a4b6b3611788a5
Rezi Karanadze <karanadzerevazi@gmail.com>
2025-04-02 15:45:11 +0400

    implement router

---
+++ src/routes/router.tsx
@@ -0,0 +1,46 @@
+import {
+  createRouter,
+  Outlet,
+  createRootRoute,
+  Route,
+} from "@tanstack/react-router";
+import { lazy } from "react";
+import { AuthProvider } from "../context/AuthContext";
+
+const Root = () => (
+  <AuthProvider>
+    <Outlet />
+  </AuthProvider>
+);
+
+const rootRoute = createRootRoute({
+  component: Root,
+});
+
+const indexRoute = new Route({
+  getParentRoute: () => rootRoute,
+  path: "/",
+  component: () => <div>Home! (redirect to /dashboard soon)</div>,
+});
+
+const loginRoute = new Route({
+  getParentRoute: () => rootRoute,
+  path: "/login",
+  component: lazy(() => import("../pages/Login")),
+});
+
+const dashboardRoute = new Route({
+  getParentRoute: () => rootRoute,
+  path: "/dashboard",
+  component: lazy(() => import("../pages/Dashboard")),
+});
+
+const routeTree = rootRoute.addChildren([
+  indexRoute,
+  loginRoute,
+  dashboardRoute,
+]);
+
+const router = createRouter({ routeTree });
+
+export default router;
